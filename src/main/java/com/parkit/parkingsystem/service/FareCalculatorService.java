c11f79e6b4fac3d76fbafc2750c6d04999cf4b79
LOPES Thomas <thomaslopes001@gmail.com>
2024-12-28 23:26:11 +0100

    Change getHours() to getTime() and duration calculation

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -7,22 +7,28 @@ public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        long inTimeHour = ticket.getInTime().getTime();
+        long outTimeHour = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        double duration = (outTimeHour - inTimeHour) / (1000.0 * 60 * 60);
+
+        if (duration <= 0) {
+            throw new IllegalArgumentException("Duration must be positive: " + duration);
+        }
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                System.out.println("Price calculated for CAR: " + ticket.getPrice());
                 break;
             }
             case BIKE: {
                 ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                System.out.println("Price calculated for BIKE: " + ticket.getPrice());
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
